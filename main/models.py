363b86a7273bf7426f5724f56a39a6f3f4206c62
Aforambis <rusydanmujtabaa@gmail.com>
2025-09-17 15:55:07 +0700

    Final commit

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class News(models.Model):
     CATEGORY_CHOICES = [
@@ -15,10 +16,12 @@ class News(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
-    thumbnail = models.URLField(blank=True, null=True)
+    thumbnail = models.URLField(max_length=1000, blank=True, null=True)
     news_views = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     is_featured = models.BooleanField(default=False)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     
     def __str__(self):
         return self.title
